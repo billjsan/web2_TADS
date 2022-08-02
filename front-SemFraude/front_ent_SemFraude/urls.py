5a0c9a5a18f892efa50b07ed428f58892178ad5c
reinando <reinando@localhost.localdomain>
2022-08-01 22:51:12 -0300

    front-end

--- front-SemFraude/front_ent_SemFraude/urls.py
+++ front-SemFraude/front_ent_SemFraude/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("visualizar/<classe>", views.visualizar, name="visualizar"),
+    path("example/", views.json_example, name="json"),
+    path("example2/", views.json_example2, name="json2"),
+
 ]
--- front-SemFraude/front_ent_SemFraude/views.py
+++ front-SemFraude/front_ent_SemFraude/views.py
@@ -1,8 +1,51 @@
+from random import random
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     return render(request, "front_ent_SemFraude/index.html")
+
+def visualizar(request,classe):
+    return render(request, "front_ent_SemFraude/Visualizar_Itens.html",{"classe":classe})
+
+@csrf_exempt
+def json_example(request):
+    data = {
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    dump = json.dumps(data)
+    return HttpResponse(dump, content_type='application/json')
+
+def json_example2(request):
+    vetor  = []
+    data = {
+        'id': 1,
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    vetor.append(data)
+    data = {
+        'id': 2,
+        'name': 'Vitor2',
+        'location': 'Finlan2d',
+        'is_active': True,
+        'count': 28
+    }
+
+    vetor.append(data)
+    
+    dump = json.dumps(vetor)
+    return HttpResponse(dump, content_type='application/json')
